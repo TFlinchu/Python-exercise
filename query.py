482f23175ddfacee55e815e095c29e4883a496e8
Tyler Flinchum <tflinchu@emich.edu>
2023-11-02 15:26:57 +0000

    add: option to list all movie names
    This adds the option '1' to the menu, and will list all the movie names in movies.txt
    
    Issue #2

--- query.py
+++ query.py
@@ -4,12 +4,16 @@ if __name__ == "__main__":
     movies = Movies('./movies.txt')
     
     while True:
-        print("Menu:")
+        print("\nMenu:")
+        print("1. List all movie names")
         print("q. Quit")
         
         choice = input("Enter your choice: ")
-        
-        if choice == 'q':
+        print("")
+        if choice == '1':
+            for movie in movies._movies:
+                print(movie['name'])
+        elif choice == 'q':
             break
         else:
             print("Invalid choice. Please try again.")
