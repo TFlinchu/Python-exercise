cd1e18b868855eb42397e26f6d9e9023687307a6
Tyler Flinchum <tflinchu@emich.edu>
2023-11-02 15:45:27 +0000

    add: option to search for movies by actor
    Adds the option in the menu to search for movies by their actors.
    note: It doesn't necessarily adhere to the expected output, but I believe this looks better.
    
    Issue #4

--- query.py
+++ query.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
         print("\nMenu:")
         print("1. List all movie names")
         print("2. Search movies by name")
+        print("3. Search movies by cast")
         print("q. Quit")
         
         choice = input("Enter your choice: ")
@@ -23,6 +24,24 @@ if __name__ == "__main__":
             if found_movies:
                 for movie in found_movies:
                     print(movie['name'])
+            else:
+                print("No matching movies found.")
+
+        elif choice == '3':
+            search_term = input("Enter the search term: ").lower()
+            print("")
+            matching_movies = []
+
+            for movie in movies._movies:
+                matching_cast = [actor for actor in movie['cast'] if search_term in actor.lower()]
+                if matching_cast:
+                    matching_movies.append((movie['name'], matching_cast))
+
+            if matching_movies:
+                for movie, cast in matching_movies:
+                    print(movie)
+                    print(cast)
+                    print("")
             else:
                 print("No matching movies found.")
 
